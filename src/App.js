d7e43ef3b39f4af5525ba9a139bd7769dbcd8474
AlvifSandana <asm240800@gmail.com>
2020-11-05 21:42:46 +0700

    add some particles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import TicTacToe from "./TicTacToe";
 import "papercss/dist/paper.min.css";
+import Particles from "react-particles-js";
 
 function App() {
   return (
@@ -11,12 +12,18 @@ function App() {
       </Main>
       <Footer>
         <FooterInner>
-          View the code on{" "}
+          Thanks to{" "}
           <a href="https://github.com/Clarity-89/React_tic_tac_toe">Github</a>
         </FooterInner>
       </Footer>
     </>
   );
+}
+
+class App extends Component {
+  render() {
+    return <Particles className="particles" params={particlesOptions} />;
+  }
 }
 
 const Main = styled.main`
@@ -37,4 +44,17 @@ const Footer = styled.footer`
 const FooterInner = styled.div`
   padding: 16px 0;
 `;
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+
 export default App;
